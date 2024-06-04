ba9debfdac2d1f945a5ec7b7140a3865f32938c3
Sihyun <71445573+sihyunPJ@users.noreply.github.com>
2024-06-04 13:49:29 +0900

    Update main.py

--- gradio/main.py
+++ gradio/main.py
@@ -11,6 +11,6 @@ t2i_generator_tab, i2i_generator_tab, ip_generator_tab = gradio_ui_tab(t2i_gener
 demo = gr.TabbedInterface([t2i_generator_tab, i2i_generator_tab, ip_generator_tab], ["Text to Image", "Image to Image", "Inpainting"])
 
 if __name__ == "__main__":
-    demo.launch(debug=True, share=True,server_port=8000)
+    demo.launch(debug=True, share=True)
 
 
