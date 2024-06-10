c5bfa002d1522e2d2c7860f8abc990a8d6b6c9df
Sihyun <71445573+sihyunPJ@users.noreply.github.com>
2024-06-10 18:54:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ from diffusers import StableDiffusionPipeline, StableDiffusionXLImg2ImgPipeline,
 import torch
 
 # model_id = "dreamlike-art/dreamlike-photoreal-2.0"
-# model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-model_id = "runwayml/stable-diffusion-v1-5"
+model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+# model_id = "runwayml/stable-diffusion-v1-5"
 
 lms = LMSDiscreteScheduler(
     beta_start=0.00085,
@@ -75,22 +75,22 @@ t2i_pipe = t2i_pipe.to("cuda")
 
 
 # SDXL - Image to image
-# i2i_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-#     model_id,
-#     torch_dtype=torch.float16,
-#     use_safetensors=True,
-#     variant="fp16")
+i2i_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16")
 
-# i2i_pipe = i2i_pipe.to("cuda")
+i2i_pipe = i2i_pipe.to("cuda")
 
-# ip_model_id = "runwayml/stable-diffusion-inpainting"
-# ip_pipe = StableDiffusionInpaintPipeline.from_pretrained(
-#     ip_model_id,
-#     variant="fp16",
-#     use_safetensors=True,
-#     torch_dtype=torch.float16,
-# )
-# ip_pipe = ip_pipe.to("cuda")
+ip_model_id = "runwayml/stable-diffusion-inpainting"
+ip_pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    ip_model_id,
+    variant="fp16",
+    use_safetensors=True,
+    torch_dtype=torch.float16,
+)
+ip_pipe = ip_pipe.to("cuda")
 
 print("Model loaded!")
 
@@ -122,54 +122,54 @@ def t2i_generator(prompt, negative, scheduler, num_inference_steps, width, heigh
 
   return input_prompt, img
 
-# def i2i_generator(prompt, negative, input_img, num_inference_steps, width, height, cfg_scale, denoising_strength, batch_count, seed, scheduler, is_chatgpt):
-#   prompt = gen(prompt) if is_chatgpt else prompt
-#   # prompt = gen(prompt)
-#   # prompt = prompt
-#   i2i_pipe.scheduler = lms if scheduler == "lms" else dpm if scheduler == "dpm" else euler
+def i2i_generator(prompt, negative, input_img, num_inference_steps, width, height, cfg_scale, denoising_strength, batch_count, seed, scheduler, is_chatgpt):
+  prompt = gen(prompt) if is_chatgpt else prompt
+  # prompt = gen(prompt)
+  # prompt = prompt
+  i2i_pipe.scheduler = lms if scheduler == "lms" else dpm if scheduler == "dpm" else euler
 
-#   # 고정 프롬프트 설정
-#   fixed_prompt = "realistic, best quality, masterpiece, "
-#   fixed_negative = "worst, bad quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, ugly, "
-#   input_prompt = fixed_prompt + prompt
-#   negative_prompt = fixed_negative + negative
+  # 고정 프롬프트 설정
+  fixed_prompt = "realistic, best quality, masterpiece, "
+  fixed_negative = "worst, bad quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, ugly, "
+  input_prompt = fixed_prompt + prompt
+  negative_prompt = fixed_negative + negative
 
-#   img = i2i_pipe(
-#     prompt = input_prompt,
-#     negative_prompt=fixed_negative + negative,
-#     image=input_img,
-#     num_inference_steps=num_inference_steps,
-#     width=width,
-#     height=height,
-#     guidance_scale = cfg_scale,
-#     strength=denoising_strength,
-#     num_images_per_prompt=batch_count,
-#     generator=torch.Generator(device="cuda").manual_seed(seed)
-#   ).images[0]
+  img = i2i_pipe(
+    prompt = input_prompt,
+    negative_prompt=fixed_negative + negative,
+    image=input_img,
+    num_inference_steps=num_inference_steps,
+    width=width,
+    height=height,
+    guidance_scale = cfg_scale,
+    strength=denoising_strength,
+    num_images_per_prompt=batch_count,
+    generator=torch.Generator(device="cuda").manual_seed(seed)
+  ).images[0]
 
-#   return input_prompt, img
+  return input_prompt, img
 
-# def ip_generator(prompt, negative, input_img, mask_img, num_inference_steps, width, height, cfg_scale, denoising_strength, batch_count, seed, scheduler, is_chatgpt):
-#   fixed_prompt = "realistic, best quality, masterpiece, "
-#   fixed_negative = "worst, bad quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, ugly, "
-#   input_prompt = fixed_prompt + prompt
-#   negative_prompt = fixed_negative + negative
+def ip_generator(prompt, negative, input_img, mask_img, num_inference_steps, width, height, cfg_scale, denoising_strength, batch_count, seed, scheduler, is_chatgpt):
+  fixed_prompt = "realistic, best quality, masterpiece, "
+  fixed_negative = "worst, bad quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, ugly, "
+  input_prompt = fixed_prompt + prompt
+  negative_prompt = fixed_negative + negative
 
-#   img = ip_pipe(
-#     prompt = input_prompt,
-#     negative_prompt=fixed_negative + negative,
-#     image=input_img,
-#     mask_image=mask_img,
-#     num_inference_steps=num_inference_steps,
-#     width=width,
-#     height=height,
-#     guidance_scale = cfg_scale,
-#     strength=denoising_strength,
-#     num_images_per_prompt=batch_count,
-#     generator=torch.Generator(device="cuda").manual_seed(seed)
-#   ).images[0]
+  img = ip_pipe(
+    prompt = input_prompt,
+    negative_prompt=fixed_negative + negative,
+    image=input_img,
+    mask_image=mask_img,
+    num_inference_steps=num_inference_steps,
+    width=width,
+    height=height,
+    guidance_scale = cfg_scale,
+    strength=denoising_strength,
+    num_images_per_prompt=batch_count,
+    generator=torch.Generator(device="cuda").manual_seed(seed)
+  ).images[0]
 
-#   return input_prompt, img
+  return input_prompt, img
 
 
 
@@ -188,46 +188,46 @@ t2i_generator_tab = gr.Interface(fn=t2i_generator,
                     , outputs=["text", "image"]
                     , title = "Innople AI Studio")
 
-# i2i_generator_tab = gr.Interface(fn=i2i_generator,
-#                                  inputs=["textbox",
-#                                          "textbox",
-#                                           gr.Image(value=None, type="pil"),
-#                                           gr.Slider(1, 150, step=1, value=20, label="Sampling Steps", show_label=True ),
-#                                           gr.Slider(64, 2048, step=8, value=512, label="Img Width", show_label=True ),
-#                                           gr.Slider(64, 2048, step=8, value=512, label="Img Height", show_label=True),
-#                                           gr.Slider(1, 30, step=0.5, value=7, label="CFG Scale", show_label=True),
-#                                           gr.Slider(0, 1, step=0.01, value=0.75, label="Denoising Strength", show_label=True ),
-#                                           gr.Slider(1, 10, step=1, value=1, label="Batch count", show_label=True),
-#                                           gr.Slider(1, 99999, step=1, value=1, label="Seed", show_label=True),
-#                                           gr.Dropdown(["lms", "dpm", "euler"], label="Sampling method", value="lms"),
-#                                           "checkbox"
-#                                           ]
-#                     , outputs=["text", "image"]
-#                     , title = "Innople AI Studio")
+i2i_generator_tab = gr.Interface(fn=i2i_generator,
+                                 inputs=["textbox",
+                                         "textbox",
+                                          gr.Image(value=None, type="pil"),
+                                          gr.Slider(1, 150, step=1, value=20, label="Sampling Steps", show_label=True ),
+                                          gr.Slider(64, 2048, step=8, value=512, label="Img Width", show_label=True ),
+                                          gr.Slider(64, 2048, step=8, value=512, label="Img Height", show_label=True),
+                                          gr.Slider(1, 30, step=0.5, value=7, label="CFG Scale", show_label=True),
+                                          gr.Slider(0, 1, step=0.01, value=0.75, label="Denoising Strength", show_label=True ),
+                                          gr.Slider(1, 10, step=1, value=1, label="Batch count", show_label=True),
+                                          gr.Slider(1, 99999, step=1, value=1, label="Seed", show_label=True),
+                                          gr.Dropdown(["lms", "dpm", "euler"], label="Sampling method", value="lms"),
+                                          "checkbox"
+                                          ]
+                    , outputs=["text", "image"]
+                    , title = "Innople AI Studio")
 
-# ip_generator_tab = gr.Interface(fn=ip_generator,
-#                                  inputs=["textbox",
-#                                          "textbox",
-#                                           gr.Image(value=None, type="pil"),
-#                                           gr.Image(value=None, type="pil"),
-#                                           gr.Slider(1, 150, step=1, value=20, label="Sampling Steps", show_label=True ),
-#                                           gr.Slider(64, 2048, step=8, value=512, label="Img Width", show_label=True ),
-#                                           gr.Slider(64, 2048, step=8, value=512, label="Img Height", show_label=True),
-#                                           gr.Slider(1, 30, step=0.5, value=7, label="CFG Scale", show_label=True),
-#                                           gr.Slider(0, 1, step=0.01, value=0.75, label="Denoising Strength", show_label=True ),
-#                                           gr.Slider(1, 10, step=1, value=1, label="Batch count", show_label=True),
-#                                           gr.Slider(1, 99999, step=1, value=1, label="Seed", show_label=True),
-#                                           gr.Dropdown(["lms", "dpm", "euler"], label="Sampling method", value="lms"),
-#                                           "checkbox"
-#                                           ]
-#                     , outputs=["text", "image"]
-#                     , title = "Innople AI Studio")
+ip_generator_tab = gr.Interface(fn=ip_generator,
+                                 inputs=["textbox",
+                                         "textbox",
+                                          gr.Image(value=None, type="pil"),
+                                          gr.Image(value=None, type="pil"),
+                                          gr.Slider(1, 150, step=1, value=20, label="Sampling Steps", show_label=True ),
+                                          gr.Slider(64, 2048, step=8, value=512, label="Img Width", show_label=True ),
+                                          gr.Slider(64, 2048, step=8, value=512, label="Img Height", show_label=True),
+                                          gr.Slider(1, 30, step=0.5, value=7, label="CFG Scale", show_label=True),
+                                          gr.Slider(0, 1, step=0.01, value=0.75, label="Denoising Strength", show_label=True ),
+                                          gr.Slider(1, 10, step=1, value=1, label="Batch count", show_label=True),
+                                          gr.Slider(1, 99999, step=1, value=1, label="Seed", show_label=True),
+                                          gr.Dropdown(["lms", "dpm", "euler"], label="Sampling method", value="lms"),
+                                          "checkbox"
+                                          ]
+                    , outputs=["text", "image"]
+                    , title = "Innople AI Studio")
 
 
 
-# demo = gr.TabbedInterface([t2i_generator_tab, i2i_generator_tab, ip_generator_tab], ["Text to Image", "Image to Image", "Inpainting"])
+demo = gr.TabbedInterface([t2i_generator_tab, i2i_generator_tab, ip_generator_tab], ["Text to Image", "Image to Image", "Inpainting"])
 # demo = gr.TabbedInterface([t2i_generator_tab, i2i_generator_tab], ["Text to Image", "Image to Image"])
-demo = t2i_generator_tab
+# demo = t2i_generator_tab
 
 
 
